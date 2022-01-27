8ebd8872587a76cb33047e9b01a9c357dc3adff6
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2022-01-27 14:25:39 +0000

    [LeetCode Sync] Runtime - 1 ms (9.70%), Memory - 43.6 MB (99.89%)

---
+++ 0031-next-permutation/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i;
+        for( i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1])
+                break;
+        }
+        if(i<0){
+            i=0;
+              for(i=0;i<nums.length/2;i++){
+            swap(nums,i,nums.length-i-1);
+        }
+        }
+        else{
+            int k=i;
+            int j;
+            for(j=nums.length-1;j>i;j--){
+                if(nums[j]>nums[i]){
+                    break;
+                }
+            }
+              swap(nums,j,i);
+            int l=0;
+              for(i=k+1;i<nums.length-1;i++){
+                  for(int m=i+1;m<nums.length;m++){
+                      if(nums[i]>nums[m])
+                        swap(nums,i,m);
+                
+                  }
+                
+               }
+        }
+      
+    }
+    void swap(int nums[], int j, int i){
+        int temp=nums[j];
+        nums[j]=nums[i];
+        nums[i]=temp;
+    }
+}
