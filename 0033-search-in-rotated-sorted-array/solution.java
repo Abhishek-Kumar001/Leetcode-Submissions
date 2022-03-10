4917c7f6946bb6d02a6faf6eca5f459a712afbc8
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2022-03-10 13:47:30 +0000

    [LeetCode Sync] Runtime - 1 ms (1.95%), Memory - 42.6 MB (99.97%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -1,25 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0;  int end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
+        int index=0;
+        if(nums[0]>nums[nums.length-1]){
+           index=pivote(nums);
+       
+           if(target>=nums[0] && target<=nums[index])
+               return  binarySearch(nums,target,0,index);
+           else if(target>=nums[index+1] && target<=nums[nums.length-1])
+                return binarySearch(nums,target,index+1,nums.length-1); 
+           else   return -1;
+        }
+        else
+           return  binarySearch(nums,target,0,nums.length-1);
+       // return -1;
+    }
+    public int pivote(int a[]){
+      int s=0,e=a.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(a[mid]>a[mid+1])
+                return mid;
+            else if(a[mid]>=a[0])
+                  s=mid+1;
+            else    
+                  e=mid-1;
+        }
+        return -1;
+    }
+    public int binarySearch(int nums[],int target,int s,int e){
+        while(s<=e){
+            int mid=s+(e-s)/2;
             if(nums[mid]==target)
                 return mid;
-            if(nums[start]<=nums[mid]){
-                if(target>=nums[start] && target < nums[mid])
-                    end=mid-1;
-                else{
-                    start=mid+1;
-                }
-            }
-            else{
-                if(target>nums[mid] && target <=nums[end]){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
+            else if(nums[mid]>target)
+                e=mid-1;
+            else
+                s=mid+1;
         }
         return -1;
     }
