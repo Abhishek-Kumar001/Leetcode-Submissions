13e805b68b4c61b890c742bc602978da2c9abbff
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2022-03-14 03:47:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47.3 MB (100.00%)

--- 0882-peak-index-in-a-mountain-array/solution.java
+++ 0882-peak-index-in-a-mountain-array/solution.java
@@ -3,26 +3,29 @@ class Solution {
        int start=0;
         int end=arr.length-1;
         int mid=0;
-      //  while(start<=end) {
-       //    mid=start+(end-start) /2;
+      // 
+        
+        while(start<=end) {
+          mid=start+(end-start) /2;
+        
+            if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) 
+                return mid;
+            else if(arr[mid]>arr[mid+1]) 
+            end=mid;
+            else
+                start=mid+1;
+        }
+        return mid;
+        
+        
         //
-            //if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) 
-        //        return mid;
-     //       else if(arr[mid]>arr[mid+1]) 
-       //         end=mid-1;
-      //      else
-      //          start=mid+1;
-     //   }
-     //   return mid;
-        
-        
-        while(start<end){
+        /* while(start<end){
            mid=start+(end-start) /2;
        if(arr[mid]>arr[mid+1]) 
            end=mid;
        else
            start=mid+1;
         }
-        return start;
+        return start;    */
     }
 }
