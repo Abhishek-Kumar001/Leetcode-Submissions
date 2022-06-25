856f9f899734c80e04c16c9309498e98f3d229c7
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2022-06-25 07:39:34 +0000

    [LeetCode Sync] Runtime - 1 ms (3.58%), Memory - 43.3 MB (99.93%)

--- 0021-merge-two-sorted-lists/solution.java
+++ 0021-merge-two-sorted-lists/solution.java
@@ -10,14 +10,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode l1=list1;
+      // HINT: SOLVE BY 2 POINTERS APPROACH
+        ListNode l1=list1;
         ListNode l2=list2;
+        
+        //if l1 or l2 is null or both are null 
+        if(l1 == null || l2 == null)
+           return l1==null ? l2 : l1;
+        
         //creating a newList
         ListNode Head=null;
         ListNode curr=null;
        
         while(l1 != null && l2 != null){
-            
+                //if list1 val is smaller  
             if(l1.val<l2.val){
                  //creating a temp node
                  ListNode temp=new ListNode(l1.val);
@@ -30,60 +36,28 @@ class Solution {
                     curr.next=temp;
                     curr=curr.next;
                 }
-                
                 //increment l1
                 l1=l1.next;
             }
+                 //  if list2 val is smaller or equal
             else{
                   //creating a temp node
                  ListNode temp=new ListNode(l2.val);
                 //if ll is empty
                 if(Head==null){
-                    curr=temp;
-                    Head=temp;
+                    curr=temp;     
+                    Head=temp;         
                 }
                 else{
                     curr.next=temp;
                     curr=curr.next;
                 } 
-                
                 //increment l2
                 l2=l2.next;
             }
-        }
-        
-        while(l1 != null){
-             //creating a temp node
-                 ListNode temp=new ListNode(l1.val);
-           //if ll is empty
-                if(Head==null){
-                    curr=l1;
-                    Head=l1;
-                }
-                else{
-                    curr.next=l1;
-                    curr=curr.next;
-                }
-                
-                //increment l1
-                l1=l1.next;
-        }
-        while(l2  != null){
-              //creating a temp node
-                 ListNode temp=new ListNode(l2.val);
-                //if ll is empty
-                if(Head==null){
-                    curr=l2;
-                    Head=l2;
-                }
-                else{
-                    curr.next=l2;
-                    curr=curr.next;
-                } 
-                
-                //increment l2
-                l2=l2.next;
-        }
+        } 
+
+        curr.next=l1 == null ? l2 :l1 ;
         return Head;
     }
 }
