c48a1da8059efbdb760fe66e3a176356134973d1
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2022-07-14 13:12:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (99.67%)

---
+++ 0795-k-th-symbol-in-grammar/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if( n == 1 && k == 1){
+            return 0;
+        }
+        int mid = ( (int) Math.pow(2, n-1) ) / 2;
+        
+        if(k<= mid){
+          return  kthGrammar( n-1 , k);
+        }
+        else{
+           int ans = ( kthGrammar( n-1, k-mid ) );
+            if(ans==1)
+                return 0;
+            else
+                return 1;
+        }
+    }
+    
+}
