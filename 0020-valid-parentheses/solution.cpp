bb3a9c9f8bfc8607ce7806dd1ee270a3a57e338f
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2023-09-14 10:56:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.7 MB (100.00%)

---
+++ 0020-valid-parentheses/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                stk.push(s[i]);
+            }else{
+               if(stk.empty()){
+                   return false;
+               }
+                char top = stk.top();
+               
+                if( (s[i] == ')' && top =='(' )  ||   ( s[i]=='}' && top=='{'  )  ||  ( s[i] == ']'  &&                                                                                                    top=='['  ) ){
+                     stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if( stk.size() == 0 ){
+            return true;
+        }
+        return  false;
+    }
+};
