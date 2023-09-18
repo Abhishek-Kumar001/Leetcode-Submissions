3e8a52b96e87ade3ae8dd2a5309786f6ee6c8fe3
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2023-09-18 12:05:58 +0000

    [LeetCode Sync] Runtime - 92 ms (6.36%), Memory - 93.7 MB (99.98%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit =0;
+        int miniSoFar = prices[0];
+
+        for(int i=1; i<prices.size(); i++){
+             profit = max( profit, prices[i] - miniSoFar);
+             miniSoFar = min(miniSoFar, prices[i]);
+        }
+        return profit;
+    }
+};
