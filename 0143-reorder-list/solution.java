abfbe8365231a68d556d62fd150d98c41efaadfe
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2023-09-20 16:40:41 +0000

    [LeetCode Sync] Runtime - 1 ms (99.91%), Memory - 45.6 MB (100.00%)

--- 0143-reorder-list/solution.java
+++ 0143-reorder-list/solution.java
@@ -9,44 +9,71 @@
  * }
  */
 class Solution {
+    public ListNode reverse( ListNode head){
+        ListNode prev = null;
+        ListNode nextNode = null;
+        while( head != null){
+            nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
     public void reorderList(ListNode head) {
-              //counting no of nodes
-        ListNode temp=head;
-        int count=0;
-        while(temp != null){
-            count++;
-            temp=temp.next;
-        }
-        //creating a arrayList of given linked list size
-        ArrayList<ListNode> al=new ArrayList(count);
-       
-        //now storing all link address in the ArrayList
-        temp=head;
-        while(temp != null){
-            al.add(temp);
-            temp=temp.next;
-        }
-        //now swap the only nodes not data a/c to given ordar(IN QUESTION)
-        temp=head;
+        //      #####@@@@ same prev submited code with more added comments
 
-        for(int i=0;i<count/2; i++){ 
-           temp.next=al.get(count-1-i);
-            if(i != (count/2) - 1){
-               temp.next.next=al.get(i + 1);
-               temp=temp.next.next;   
-            }
-            else{
-               temp= temp.next;
-            }
+        // soluiton:- the app is hm 2nd half ko reverse kardenge and 2 pointer chala ke nodes ke link ko change
+        //            kar denge
+
+        // first count the no of nodes
+        ListNode temp = head;
+        int size =0;
+        while(temp != null){
+            size++;
+            temp = temp.next;
         }
-       //managing last node  
-        //if no of nodes is even the add middle node 1st in the ll then point last node to null
-        if(count%2 ==1){
-            temp.next=al.get(count/2);
-            temp.next.next=null;
+
+        // find the mid and reverse the second half
+        ListNode fast = head;
+        ListNode slow = head;
+        while( fast != null  && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        else{       //if no of ll is even only set last node to null
-            temp.next=null;
-        }    
+        // here solw containt the mid node
+        ListNode end = reverse( slow );
+        ListNode start = head;
+
+        // apply 2 pointer and link nodes a/c to question
+        ListNode ansNode =  new ListNode(-1);// creating a dummy node
+        ListNode ansHead = ansNode;
+
+        int cnt =0;
+        while( cnt < size/2){ // go till size/2 and link nodes a/c to question
+            //  hme line no-57 i.e start = stert.next line no 59 ke pahle likhna hi hoga
+            // qunki line no 59 i.e( ansNode.next.next) start ke next pointer ke chage kar rha
+            ansNode.next = start;
+            start = start.next;
+
+            ansNode.next.next= end;
+            end = end.next;
+
+            ansNode = ansNode.next.next;
+  
+            cnt++;
+        }
+        // if we have even no of nodes given in qs then explicitely link the middle node
+        if( size % 2 == 1){
+            ansNode.next = start;
+            ansNode = ansNode.next;
+        }
+        // now poin the last node to null other wise we fill fall in cycle
+
+        //if( ansNode != null)  // check karne ki koi jarurat nahi hai
+          ansNode.next = null;
+
+        // now updat the head b/c hame given ll ko ki modify kar ke return karna tha
+        head = ansHead.next;  // ansHead.next b/c ansHead dummy node (i.e -1 ) hold kiye hue hai 
     }
 }
