379d5360c2da20b676b895a74ece65c376802515
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2023-10-15 04:52:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (100.00%)

--- 0377-combination-sum-iv/solution.cpp
+++ 0377-combination-sum-iv/solution.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public:
     int solve( vector<int> &nums, int target, int n, vector<int> &dp){
@@ -21,5 +22,40 @@ public:
         dp[0] = 1;
 
         return  solve( nums, target,n,dp);
+    }
+};
+*/
+
+
+class Solution {
+    public:
+    int solve( vector<int> &nums, int ind,  int target, int n,vector<vector<int>> &dp ){
+         // base case
+         if( target == 0){
+             return 1;
+         }
+
+         if( ind >= n){
+             return 0;
+         }
+         
+         if( dp[ind][target] != -1) return dp[ind][target];
+
+         int take = 0;
+         if( nums[ind] <= target){
+             take = solve(nums, 0, target-nums[ind], n, dp);
+         }
+         int notTake = solve(nums, ind+1, target, n, dp);
+
+         return dp[ind][target] = take + notTake;
+        
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        // code with alisha appproah
+        int n  = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+
+        return  solve( nums,0, target,n,dp);
     }
 };
