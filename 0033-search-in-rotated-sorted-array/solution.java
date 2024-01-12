ecc51c812c1295317157426b7fcc3db9fe5fc3a0
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-01-12 15:53:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (99.97%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -1,42 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index=0;
-        if(nums[0]>nums[nums.length-1]){
-           index=pivote(nums);
-       
-           if(target>=nums[0] && target<=nums[index])
-               return  binarySearch(nums,target,0,index);
-           else if(target>=nums[index+1] && target<=nums[nums.length-1])
-                return binarySearch(nums,target,index+1,nums.length-1); 
-           else   return -1;
-        }
-        else
-           return  binarySearch(nums,target,0,nums.length-1);
-       // return -1;
-    }
-    public int pivote(int a[]){
-      int s=0,e=a.length-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(a[mid]>a[mid+1])
+        int s=0, e=nums.length-1;
+        
+        int ansInd =0;
+        while( s <= e){
+            int mid = (s+e)/2;
+            if( nums[mid] == target){
                 return mid;
-            else if(a[mid]>=a[0])
-                  s=mid+1;
-            else    
-                  e=mid-1;
+            }
+            
+            if( nums[s] <= nums[mid]){
+                // it means 1s  half is sorted so apply binary search logic to find
+                if( target >= nums[s]  && target <= nums[mid]){
+                     e = mid-1;
+                }
+                else{
+                    //ans lies in 2nd(unsorted half)
+                    s = mid+1;
+                }
+            }
+            else{
+                // 2nd half is sorted
+                if( target >= nums[mid]  && target <= nums[e]){
+                      s = mid+1;
+                }
+                else{
+                    e = mid -1;
+                }
+            }
         }
         return -1;
-    }
-    public int binarySearch(int nums[],int target,int s,int e){
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]>target)
-                e=mid-1;
-            else
-                s=mid+1;
-        }
-        return -1;
+        
     }
 }
