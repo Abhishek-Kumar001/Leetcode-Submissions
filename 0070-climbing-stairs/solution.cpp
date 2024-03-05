c3192e92b9b467c39b2d5b0a91f1e9b0c2ced223
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-03-05 06:33:32 +0000

    [LeetCode Sync] Runtime - 3 ms (0.34%), Memory - 7.4 MB (100.00%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,23 +1,17 @@
 class Solution {
-    private:
-    int helper(int n, vector<int> &dp){
-        // base case
-        if( n < 0) return 0;
-        if( n == 1 || n == 0) return 1;
-
-        //memo
-        if( dp[n] != -1) return dp[n];
-
-        int one = helper(n-1, dp);
-        int two = helper(n-2, dp);
-        
-        return dp[n] = one + two;
-
-    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-        return helper( n, dp);
-
+        return solve(n, dp);
+    }
+    
+    int solve( int n , vector<int> &dp){
+        if(n == 2) return 2;
+        if(n == 1) return 1;
+        
+        if( dp[n] != -1) return dp[n];
+        
+        dp[n] =  solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
     }
 };
