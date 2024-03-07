e00e88724b7017e98760d170bc4d9921e9e83aec
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-03-07 10:08:47 +0000

    [LeetCode Sync] Runtime - 5 ms (0.51%), Memory - 9.5 MB (100.00%)

--- 0213-house-robber-ii/solution.cpp
+++ 0213-house-robber-ii/solution.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    int helper(vector<int>& nums) {
-        // my code space optmization
-        int n = nums.size();
-        if( n == 1) return nums[0];
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for(int i=2; i<n; i++){
-            dp[i] = max( dp[i-1] , nums[i]+dp[i-2]);
-        }
-
-        return dp[n-1];
-    }
     int rob(vector<int>& nums) {
-        // only a little change in House Robber -1    
-
-        int n = nums.size();
-        // corner case
-        if( n == 1) return nums[0];
-
-        vector<int> nums1(n-1);
-        vector<int> nums2(n-1);
-
-        for(int i=0; i<n; i++){
-            if( i != 0){
-                nums1.push_back( nums[i] );
-            }
-            if( i != n-1){
-                nums2.push_back( nums[i] );
-            }
+        if( nums.size() == 1) return nums[0];
+        
+        vector<int> nums1, nums2;
+        for( int i=0; i<nums.size(); i++){
+            if( i != nums.size()-1) nums1.push_back( nums[i]);
+            if( i != 0) nums2.push_back( nums[i]);       
         }
-        return max( helper(nums1), helper(nums2));
+        return max( solve(nums1), solve(nums2));
+    }
+    int solve( vector<int> nums){
+        int prev1= nums[0];
+        if( nums.size() == 1) return nums[0];
+        int prev2 = max(nums[0], nums[1]);
+        
+        for( int i=2;i<nums.size(); i++){
+            int curr = max( prev2,  nums[i]+ prev1);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
     }
 };
