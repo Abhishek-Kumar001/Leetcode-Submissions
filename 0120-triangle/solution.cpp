34993eaad702bf8c5a88102c07f5a82d93e9c461
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-03-13 15:35:50 +0000

    [LeetCode Sync] Runtime - 7 ms (3.13%), Memory - 11.1 MB (100.00%)

---
+++ 0120-triangle/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n= triangle.size();
+        vector<int> prev(n), curr(n);
+        
+        for(int i=0; i<n; i++){
+            prev[i] = triangle[n-1][i];
+        }
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                curr[j] = min( prev[j], prev[j+1]) + triangle[i][j];
+            }
+            prev = curr;
+        }
+        return prev[0];
+    }
+};
