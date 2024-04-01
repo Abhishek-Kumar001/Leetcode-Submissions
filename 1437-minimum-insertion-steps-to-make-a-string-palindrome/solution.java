5055042605c5c3d863637c3d17a194555bd38f07
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-04-01 19:25:03 +0000

    [LeetCode Sync] Runtime - 33 ms (20.37%), Memory - 45.2 MB (96.09%)

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n=s.length();
+        
+        return  n - longestPalindromeSubseq(s);
+    }
+    
+    public int longestPalindromeSubseq(String s) {
+        String t = "";
+        for( int i=s.length()-1; i>=0; i--){
+              t = t+s.charAt(i);
+        }
+        
+        
+        return longestCommonSubsequence(s, t);
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        
+        int dp[][] = new int[n+1][m+1];
+        
+        for( int i=1; i<=n; i++){
+            for( int j=1; j<=m; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max( dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return  dp[n][m];
+    }
+}
