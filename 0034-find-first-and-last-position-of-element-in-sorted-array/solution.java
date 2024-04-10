31ff15cffacd498b4b143da3a658ec370ddaedd5
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-04-10 05:30:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.9 MB (99.99%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,38 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int[] arr={-1,-1};
-         arr[0]=binarySearch(nums,target,true);
-        if(arr[0] ==-1)
-            return arr;
-         arr[1]=binarySearch(nums,target,false);
-        return arr;      
-            
-    }
-    
-    int binarySearch(int[] nums,int target, boolean isFirstPosition){
-        int start=0 ;
-        int end=nums.length-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]==target){
-                if(isFirstPosition){
-                    ans=mid;
-                    end=mid-1;
-                }
-                else{
-                    ans=mid;
-                    start=mid+1;
-                }
-            }
-            else if(nums[mid]>target){
-               end=mid-1; 
+        int n= nums.length;
+        
+        // finding 1st occurence
+        int first = -1;
+        int s=0, e = n-1;
+        while( s<=e){
+            int mid = (s+e)/2;
+
+            if( nums[mid] == target){
+                 first = mid;
+                 e =mid-1;
+            }else if( nums[mid] > target){
+                e = mid-1;
             }
             else{
-                start=mid+1;
+                s = mid+1;
             }
-                
         }
+
+        int ans[] = new int[2];
+        if( first == -1){
+            ans[0] = -1;
+            ans[1] = -1;
+             return ans;
+        }
+
+        ///  finding last occurence
+        int second = -1;
+         s=0;
+         e = n-1;
+        while( s<=e){
+            int mid = (s+e)/2;
+
+            if( nums[mid] == target){
+                 second = mid;
+                 s =mid+1;
+            }else if( nums[mid] > target){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        ans[0] = first;
+        ans[1] = second;
         return ans;
     }
 }
