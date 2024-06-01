571ece484760ca455ef2ab31881a20d64478d913
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-01 05:52:10 +0000

    [LeetCode Sync] Runtime - 555 ms (33.36%), Memory - 11.9 MB (12.99%)

--- 0079-word-search/solution.cpp
+++ 0079-word-search/solution.cpp
@@ -1,57 +1,46 @@
 class Solution {
-private:
-    bool DFS( vector<vector<char>>& board, string word, int i, int  j, int cnt){
-        if(cnt == word.size()) return true;
-
-        int m = board.size();
-        int n = board[0].size();
-        if( i== -1 || i==m || j==-1 || j==n || board[i][j] != word[cnt]){
-            return false;
-        }       
-
-        char temp = board[i][j];
-        board[i][j] ='*';
-        bool found = DFS(board,word, i-1, j, cnt+1) || 
-                     DFS(board,word, i, j+1, cnt+1) || 
-                     DFS(board,word, i+1, j, cnt+1) || 
-                     DFS(board,word, i, j-1, cnt+1) ;
+    bool  dfs(int row , int col, vector<vector<bool>> &visited, vector<vector<char>>& grid,string                                                               word,int i, int n, int m ){
         
-        board[i][j] = temp;
-        return found;
+        visited[row][col] = true;
+       
+        // base case 
+        if( i == word.size()){
+            return true;
+        }
+        
+        int drow[] = {0, +1, 0, -1};
+        int dcol[] = {+1, 0, -1, 0};
+        
+        for (int k = 0; k < 4; k++) {  // name it k not i b/c we have use i already in parameter
+            int nrow = row + drow[k];
+            int ncol = col + dcol[k];
+            
+            if( nrow>=0 && nrow <n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow]                                    [ncol] == word[i]){
+                bool result = dfs(nrow, ncol, visited, grid, word, i+1, n, m);
+                if( result ) return true;
+            }
+     
+        }
+        visited[row][col] = false;
+        return false;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        int n= board.size(), m = board[0].size();
         
-        int m = board.size();
-        int n = board[0].size();
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j] == word[0]){
-                        bool ans = DFS( board, word, i, j , 0);
-                        if( ans) return true;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if( visited[i][j]== false && board[i][j] == word[0]){
+                    cnt++;
+                    bool result =  dfs(i, j, visited, board, word, 1, n, m);
+                    if( result) return true;
                 }
             }
+      
         }
         return false;
     }
-    
 };
-
-
-
-
-/*
-for(int i=0; i<4; i++){        
-            int nrow = row+ drow[i];
-            int ncol = col + dcol[i];
-
-            if( nrow >=0 && nrow < m && ncol >=0 && ncol <n){
-                if( board[nrow][ncol] == word[cnt]){
-                    ans=  DFS(board, word, nrow, ncol, cnt+1);
-                   // if(ans) return true;
-                }
-            }      
-}
-
-*/
