64eae142193c0afb2f8805e924953ad8d5e6f9d2
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-02 18:04:38 +0000

    [LeetCode Sync] Runtime - 32 ms (32.38%), Memory - 36 MB (100.00%)

---
+++ 0442-find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int i=0, n=nums.size();
+        while( i<n){
+            int correctPosi = nums[i] - 1;
+            if( nums[i] != nums[correctPosi]){
+                swap(nums[i], nums[correctPosi]);
+            }else{
+                i++;
+            }
+        }
+
+        vector<int> ans;
+        for( int i=0; i<n; i++){
+            if( nums[i] != i+1){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
