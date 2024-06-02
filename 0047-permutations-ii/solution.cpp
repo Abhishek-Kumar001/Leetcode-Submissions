21611bb021b62a652fd0289a3887a9c58d869de7
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-02 04:50:58 +0000

    [LeetCode Sync] Runtime - 31 ms (14.62%), Memory - 11.8 MB (69.02%)

--- 0047-permutations-ii/solution.cpp
+++ 0047-permutations-ii/solution.cpp
@@ -1,35 +1,24 @@
 class Solution {
-public:
-    int n;
-    set<vector<int>> st;
-    vector<int> ans1;
-    void f(int mask,vector<int>& v){
-        //If all the elements are fitted in the array ans1
-        if(mask==(1<<n)-1){
-            //then we store ans1 vector in the set (why set ? because it will prune the duplicate vectors) 
-            st.insert(ans1);
+    void solve(int ind, vector<int> &nums, set<vector<int>> &ansSet){
+        if(ind == nums.size()){
+            ansSet.insert(nums);
             return;
         }
-        for(int i=0;i<n;i++){
-            //checking if ith number in the array is not used in the permutation
-            if((mask&(1<<i))==0){
-                //pick
-                ans1.push_back(v[i]);
-                f(mask^(1<<i),v);
-                //drop (pick and drop are part of backtracking)
-                ans1.pop_back();
-            }
+        
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            solve(ind+1, nums, ansSet);
+            swap(nums[ind], nums[i]);
         }
-        return ;
     }
+public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        n = nums.size();
-        f(0,nums);
-        vector<vector<int>> ans;
-        //storing all the elements of the set in the ans vector of vector since return type is vector of vector
-        for(auto it:st){
-            ans.push_back(it);
+        set<vector<int>> ansSet;
+        solve(0, nums, ansSet);
+        vector<vector<int>> ansVec;
+        for( auto it: ansSet){
+            ansVec.push_back(it);
         }
-        return ans;
+        return ansVec;
     }
 };
