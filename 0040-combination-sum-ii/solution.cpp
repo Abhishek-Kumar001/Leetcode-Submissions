1fba51a8fc0a650c99cf3ccf8271e1a2d478a471
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-02 14:27:23 +0000

    [LeetCode Sync] Runtime - 5 ms (12.32%), Memory - 13.9 MB (98.92%)

--- 0040-combination-sum-ii/solution.cpp
+++ 0040-combination-sum-ii/solution.cpp
@@ -1,28 +1,33 @@
 class Solution {
-public:
-
-    void solve(vector<vector<int>> &ans, vector<int> temp, vector<int>& candidates, int require, int index ){
-        if(require == 0){
+    void solve(int ind, vector<int>& candidates, int target, vector<vector<int>> &ans, vector<int> temp)
+    {
+        //base case 
+        if( target == 0){
             ans.push_back(temp);
-            return;
+            return ;
         }
 
-        for(int i = index; i<candidates.size(); i++){
-            if(i >index &&  candidates[i] == candidates[i-1]){
+        
+
+        for( int i=ind; i<candidates.size(); i++){
+            if(i != ind && candidates[i]== candidates[i-1]){
                 continue;
             }
-            if(require >= candidates[i]){
-                temp.push_back(candidates[i]);
-                solve(ans, temp, candidates, require-candidates[i], i+1);
-                temp.pop_back();
-            }
+
+            if(target < candidates[i] ) return ;
+
+            temp.push_back(candidates[i]);
+            solve(i+1, candidates, target-candidates[i], ans, temp);
+            //backtrack
+            temp.pop_back();
         }
     }
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int>temp;
         sort(candidates.begin(), candidates.end());
-        solve(ans, temp, candidates, target, 0);
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0, candidates, target, ans, temp);
         return ans;
     }
 };
