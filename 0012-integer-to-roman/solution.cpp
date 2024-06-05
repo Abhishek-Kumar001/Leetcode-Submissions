b80b2abb66bc816a1d620793dfe723b945ccce9f
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-05 05:15:18 +0000

    [LeetCode Sync] Runtime - 3 ms (65.60%), Memory - 9.9 MB (73.90%)

---
+++ 0012-integer-to-roman/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+                          "XL", "X",  "IX", "V",  "IV", "I"};
+        int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        string ans = "";
+        for (int pos = 0; num > 0; pos++) {
+            while (num >= value[pos]) {
+                ans += roman[pos];
+                num -= value[pos];
+            }
+        }
+        return ans;
+    }
+};
