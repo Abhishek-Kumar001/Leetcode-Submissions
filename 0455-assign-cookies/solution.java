21d7517aa1fdf093a71b9385da8d6c9afc86e78b
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-21 17:04:34 +0000

    [LeetCode Sync] Runtime - 8 ms (99.18%), Memory - 45.3 MB (100.00%)

---
+++ 0455-assign-cookies/solution.java
@@ -0,0 +1,14 @@
+        // FROM DISCUSSION SECTION
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gIndx = 0, gSz = g.length, sSz = s.length;
+        for(int indx = 0; indx < sSz; indx++){
+            if(gIndx < gSz && g[gIndx] <= s[indx]){
+                gIndx++;
+            }
+        }
+        return gIndx; 
+    }
+}
