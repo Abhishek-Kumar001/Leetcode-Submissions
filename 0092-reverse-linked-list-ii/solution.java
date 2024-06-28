315fe8e556bceaa226d289f042ab517a2987aa8c
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-06-28 15:45:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.99%)

--- 0092-reverse-linked-list-ii/solution.java
+++ 0092-reverse-linked-list-ii/solution.java
@@ -8,56 +8,89 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+// class Solution {
+//     public ListNode reverseBetween(ListNode head, int left, int right) {
+//         // base case
+//         if(head.next == null) return head;
+        
+//         int cnt =1;
+//         ListNode prev = head;      
+//         ListNode curr = head;
+//         while(cnt < left){
+//             prev = curr;
+//             curr= curr.next;
+//             cnt++;
+//         }
+
+//         // reverse the n(right) nodes from here(this curr node)
+//         ListNode reversedHead = reverse(curr,left, right);
+//         if(left == 1) return reversedHead;
+   
+//         prev.next = reversedHead;
+//         return head;
+
+//     }
+//     public ListNode  reverse(ListNode head,int left, int right){
+//         int cnt =left;
+//         ListNode curr = head;
+//         ListNode prev = null;
+//         ListNode nextNode = null;
+//         while(cnt <= right && curr != null){
+//             nextNode = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = nextNode;
+//             cnt++;
+//         }
+
+//         if( nextNode != null)
+//            head.next = nextNode;
+
+//         return prev;
+//     }
+// }
+
+                   // same code with romoving unnecessary  checks -
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int pos = 0;
-        ListNode pointer = head;
-        int condition = 0;
-        while(pointer != null){
-            pos++;
-            if(pos == left){
-                condition = 1;
-            }
-            if(condition == 1){
-                list.add(pointer.val);
-            }
-            if(pos == right){
-                condition = 0;
-            }
-           
-            pointer = pointer.next;
-        }
- 
-
-
-        for(int i=0;i<list.size()/2;i++){
-            int temp = list.get(i);
-            list.set(i,list.get(list.size()-i-1)) ;
-            list.set(list.size()-i-1,temp);
+        // base case
+        if(head.next == null) return head;
+        
+        int cnt =1;
+        ListNode prev = head;      
+        ListNode curr = head;
+        while(cnt < left){
+            prev = curr;
+            curr= curr.next;
+            cnt++;
         }
 
+        // reverse the n(right) nodes from here(this curr node)
+        ListNode reversedHead = reverse(curr,left, right);
+        if(left == 1) return reversedHead;
+   
+        prev.next = reversedHead;
+        return head;
 
-
-        pointer = head;
-        pos = 0;
-        int index = 0;
-        while(pointer != null){
-            pos++;
-            if(pos == left){
-                condition = 1;
-            }
-            if(condition == 1){
-                pointer.val = list.get(index);
-                index++;
-            }
-            if(pos == right){
-                condition = 0;
-            }
-           
-            pointer = pointer.next;
+    }
+    public ListNode  reverse(ListNode head,int left, int right){
+        int cnt =left;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode nextNode = null;
+        while(cnt <= right ){   //  && curr != null  // unnecessary tha
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+            cnt++;
         }
-        pointer = head;
-        return pointer;
+
+        // if( nextNode != null)               // unnecessary
+           head.next = nextNode;
+
+        return prev;
     }
 }
