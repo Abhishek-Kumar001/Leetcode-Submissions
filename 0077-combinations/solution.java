801742e0cb8ddc4ca8665e35beec6b807fe3d8e4
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-07-18 02:57:43 +0000

    [LeetCode Sync] Runtime - 15 ms (93.11%), Memory - 94.4 MB (100.00%)

--- 0077-combinations/solution.java
+++ 0077-combinations/solution.java
@@ -1,22 +1,32 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
-
-        backtrack(1, comb, res, n, k);
-        return res;
+          //@ my code 100%
+        List<List<Integer>> ansList = new ArrayList<>();
+        List<Integer> temp = new ArrayList<Integer>();
+        
+        combination(1,n,k,temp, ansList);
+       
+        return ansList;
     }
+    public void combination(int i, int n, int k, List<Integer> temp, List<List<Integer>> ansList){
 
-    private void backtrack(int start, List<Integer> comb, List<List<Integer>> res, int n, int k) {
-        if (comb.size() == k) {
-            res.add(new ArrayList<>(comb));
-            return;
+        //base case
+        if(k == 0){
+             // ansList.add(temp);   // this is wrong
+            ansList.add(new ArrayList<>(temp));
+           
+            return ;
         }
+        if(i>n) return ;
+        if(n-i+2 < k) return ;  //if we have less elements than what length list we 
+                             //we need so it is useLess to proced aheade
 
-        for (int num = start; num <= n; num++) {
-            comb.add(num);
-            backtrack(num + 1, comb, res, n, k);
-            comb.remove(comb.size() - 1);
-        }
-    } 
+        // else we have two choices either tate i or not
+        temp.add(i);
+        combination(i+1, n, k-1, temp, ansList);
+        temp.remove(temp.size()-1);
+        combination(i+1, n, k, temp, ansList);
+       
+
+    }
 }
