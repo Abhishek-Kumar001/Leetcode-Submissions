710b627a1baabb3edf9a6442f81e797ab87b011d
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-08-13 17:13:38 +0000

    [LeetCode Sync] Runtime - 15 ms (35.85%), Memory - 13 MB (35.22%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,71 +1,40 @@
-/*
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //hint from apna college
-        int start = 0;
-        int index[26];
-        int len = 0;
-        for(int i=0; i<s.length(); i++){
-            index[i] = -1;
-        }
-        for(int i=0; i<s.length(); i++){
-            char ch = s[i];
-            if(index[ch - 'a'] == -1){
-                index[ch - 'a'] = i;
-            }
-            else{  //it is already found
-                if(s[start] == s[i])
-                   len = max(len, i-start);
-                else
-                    len = max(len, i-start + 1);
-                //index[ch - 'a'] = i;
-                
-                start = i;
+        //this codde is not recomended , previous successs 
+        // submition is recomemded
+        int n = s.length();
+        if(s.length() == 0) return 0;
+
+        // skip 0th index duplicates
+        int i=0;
+        if(n >= 2){
+            if( s[0] == s[1]){
+                while(i<n && s[0] == s[i]) i++;
+                i--;
             }
         }
-        if(start == 0){
-            return s.length();
+
+        map<char, int> mp;      
+        int cnt = 0;
+        int start = i+1; // start+1
+        int maxi = 1;  // for all duplicate
+        for(i; i<s.length(); i++){
+            int ch = s[i];
+            if( mp[ch] < start){
+                mp[ch] = i+1; // assiging its index+1 ssince 
+                // map me ch[x] == 0 means ye ele nahi aaya hai
+                // but for the 1st ele hm 0(index) assing kare
+                // to ans me problem aayegi 
+          
+                int diff = (i+1) - start+ 1;
+                maxi = max(maxi , diff);
+            }else{            
+                start = mp[ch]+1;
+                mp[ch] = i+1;  // update with the new index
+            }
         }
-        return len;
-    }
-};
 
-*/
-
-
-
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-       int ans =0;
-
-       vector<int> indexArr(256, -1);
-       int cnt=0;
-       int last =-1;
-
-       for(int i=0; i<s.length(); i++){
-           int ind = s[i] ;
-           if( indexArr[ind] == -1 ){
-               // not come yet this is the first
-               cnt++;
-               ans = max(ans, i - last);
-               indexArr[ind] = i;
-           }else{
-               // already aa chuka hai ye duplicate hai
-               
-               // now the question is kya ye last ke pahle aaya hai iff yes ignore
-               if( last > indexArr[ind]){
-                   //ignore
-                   ans = max(ans, i - last);
-               }
-               else{
-                   last =indexArr[ind];
-               }
-               indexArr[ind] = i; // updating with new index 
-           }
-       }
-       return ans;
+        return maxi;
     }
 };
