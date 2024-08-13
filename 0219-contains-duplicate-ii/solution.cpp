3d5a27162f8c3c988f8aad28037e36bba8c5db42
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-08-13 18:15:07 +0000

    [LeetCode Sync] Runtime - 186 ms (9.51%), Memory - 84.3 MB (93.38%)

--- 0219-contains-duplicate-ii/solution.cpp
+++ 0219-contains-duplicate-ii/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int n = nums.size();
-        
-        for(int i=0; i<n; i++)
-        {
-            // mp.count() will tell whatever ith index that I want, have I seen it before?
-            if(mp.count(nums[i]))
-            {
-                // if I have already seen this number, then check for condition abs(i - j) <= k
-                if(abs(i-mp[nums[i]])<=k)
-                    return true;
+        map<int,int>mpp;
+        for(int i=0; i<nums.size(); i++){
+            // if(mpp.find(nums[i]) != mpp.end()){
+            //     int index=mpp[nums[i]];
+            //     int dif=abs(index-i);
+            //     if(dif <= k)
+            //     return true;
+            // }
+
+            if(mpp.count(nums[i])){
+                int index=mpp[nums[i]];
+                int dif=abs(index-i);
+                if(dif <= k)
+                return true;
             }
-            // if I have not seen this number before, insert the number with its position in the map
-            // and if the number is already present in the map, then update the position of that number
-            mp[nums[i]] = i;
+            mpp[nums[i]]=i;
         }
-        // after the complete traversal, if we don't find a pair to satisfy the condition, return false
         return false;
     }
 };
-
