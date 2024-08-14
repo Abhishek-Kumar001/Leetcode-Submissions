d0907ce296697b2181b39ce3714033a45a91b6cf
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-08-14 08:30:56 +0000

    [LeetCode Sync] Runtime - 1086 ms (5.04%), Memory - 148.7 MB (87.33%)

--- 0220-contains-duplicate-iii/solution.cpp
+++ 0220-contains-duplicate-iii/solution.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
         int n = nums.size();
+        //handling the tle test case
+        set<int> ss(nums.begin(), nums.end());
+        if(valueDiff == 0 && n == ss.size()) return false;
+
         
-        if(n == 0 || k < 0  || t < 0) return false;
-        
-        unordered_map<int,int> buckets;
-        
-        for(int i=0; i<n; ++i) {
-            int bucket = nums[i] / ((long)t + 1);
-            
-			// For negative numbers, we need to decrement bucket by 1
-			// to ensure floor division.
-			// For example, -1/2 = 0 but -1 should be put in Bucket[-1].
-			// Therefore, decrement by 1.
-            if(nums[i] < 0) --bucket;
-            
-            if(buckets.find(bucket) != buckets.end()) return true;
-            else {
-                buckets[bucket] = nums[i];
-                if(buckets.find(bucket-1) != buckets.end() && (long) nums[i] - buckets[bucket-1] <= t) return true;
-                if(buckets.find(bucket+1) != buckets.end() && (long) buckets[bucket+1] - nums[i] <= t) return true;
-                
-                if(buckets.size() > k) {
-                    int key_to_remove = nums[i-k] / ((long)t + 1);
-                    
-                    if(nums[i-k] < 0) --key_to_remove;
-                    
-                    buckets.erase(key_to_remove);
+        for(int i=0; i<n; i++){          
+            for(int j=i+1; j<=i+indexDiff && j<n; j++ ){
+                if(abs(nums[i] - nums[j])  <= valueDiff){
+                    return true;
                 }
-            }
+            }        
         }
-        
         return false;
     }
 };
