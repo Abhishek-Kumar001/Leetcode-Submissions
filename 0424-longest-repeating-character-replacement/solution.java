2aa258388ed88764dafb2607fbc9246e31ad8057
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-08-23 12:04:42 +0000

    [LeetCode Sync] Runtime - 29 ms (20.42%), Memory - 44.6 MB (99.94%)

--- 0424-longest-repeating-character-replacement/solution.java
+++ 0424-longest-repeating-character-replacement/solution.java
@@ -1,6 +1,6 @@
-class Solution {
+class Solution2 {
     public int characterReplacement(String s, int k) {
-              // @ YT - code with Ease - by Varsha code 100% easy and succcess
+      // @ YT - code with Ease - by Varsha code 100% easy and succcess
 
         Map<Character, Integer> mp = new HashMap<>();
         int maxRepetCount = 0, start=0;
@@ -8,16 +8,10 @@ class Solution {
         int maxLen = 0;
         for(int end =0; end<n; end++){
             char ch = s.charAt(end);
-            if(mp.get(ch) != null){
-               mp.put(ch, mp.get(ch)+1 );
-            }else{
-                mp.put(ch, 1);
-            }        
-
+            mp.put(ch, mp.getOrDefault(ch, 0)+1 );
+            
             maxRepetCount = Math.max(maxRepetCount, mp.get(ch));
-
             int nonRepetCnt = (end - start+1) - maxRepetCount;
-
             if(k >= nonRepetCnt ){
                 maxLen = Math.max(maxLen, (end- start + 1));
             }else{
@@ -28,3 +22,32 @@ class Solution {
         return maxLen;
     }
 }
+
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+      // @ YT - code with Ease - by Varsha code 100% easy and succcess
+
+      Map<Character, Integer> mp = new HashMap<>();
+      int  maxMatch =0, maxLen = 0, start = 0;
+      for(int i=0; i<s.length(); i++){
+        char ch = s.charAt(i);
+        mp.put(ch, mp.getOrDefault(ch, 0)+1);
+
+        maxMatch = Math.max(maxMatch, mp.get(ch));
+        int notMatch = (i-start+1) - maxMatch;
+
+        if(notMatch <= k){
+            maxLen = Math.max(maxLen, i-start+1);
+        }else{
+            mp.put(s.charAt(start), mp.get(s.charAt(start))-1);
+            start++;
+        }
+      }
+      return maxLen;
+    }
+}
+
+
+
+
