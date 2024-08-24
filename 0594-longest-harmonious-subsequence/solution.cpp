593adbbd75f36b2c66e1cabde7b84ffb433fbb9d
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-08-24 06:31:42 +0000

    [LeetCode Sync] Runtime - 37 ms (29.89%), Memory - 36.4 MB (81.93%)

--- 0594-longest-harmonious-subsequence/solution.cpp
+++ 0594-longest-harmonious-subsequence/solution.cpp
@@ -1,17 +1,41 @@
+// class Solution {
+// public:
+//     int findLHS(vector<int>& nums) {
+//         int res = 0, left = 0, right = 1, curIndex = 0;
+//         sort(nums.begin(), nums.end());
+//         while (right < nums.size()) {
+//             if (nums[right] - nums[left] == 1) {
+//                 res = max(res, right - left + 1);
+//             }
+//             while(nums[right] - nums[left] > 1){
+//                 ++left;
+//             }
+//             ++right;
+//         }
+//         return res;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        int res = 0, left = 0, right = 1, curIndex = 0;
         sort(nums.begin(), nums.end());
-        while (right < nums.size()) {
-            if (nums[right] - nums[left] == 1) {
-                res = max(res, right - left + 1);
+
+        int start =0, maxLen = 0;
+        for(int end=1; end<nums.size(); end++){
+            if( (nums[end] - nums[start]) == 1){
+               
+                  maxLen = max(maxLen, end-start+1);
             }
-            while(nums[right] - nums[left] > 1){
-                ++left;
+
+            while(nums[end] - nums[start] > 1){
+                ++start;
             }
-            ++right;
         }
-        return res;
+        return maxLen;
     }
 };
