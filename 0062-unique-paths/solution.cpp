f2d730c4896213aeea500e74ac700a8b5b528d63
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-09-07 16:05:08 +0000

    [LeetCode Sync] Runtime - 3 ms (9.57%), Memory - 7.8 MB (93.90%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> prev(n , 1);
+        vector<vector<int>> dp(m, vector<int>(n + 1, 0));
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+
         for(int i=1; i<m; i++){
-            vector<int> curr(n, 0);
-            curr[0] =1;
-            for(int j=1; j<n;j++){
-                curr[j] = prev[j] + curr[j-1];
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
-            prev = curr;
         }
-        return prev[n-1];
+        return  dp[m-1][n-1];
     }
 };
