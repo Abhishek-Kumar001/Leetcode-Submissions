767a1f14b52dc0874631d3d75269c4b750cdbabb
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2024-09-13 05:21:11 +0000

    [LeetCode Sync] Runtime - 9 ms (25.41%), Memory - 20.9 MB (100.00%)

---
+++ 0337-house-robber-iii/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    pair<int, int> helper(TreeNode* root){
+        if(root == NULL){
+            return {0,0};
+        }
+
+        pair<int, int> left = helper(root->left);
+        pair<int, int> right = helper(root->right);
+
+        int take = root->val + left.second + right.second;
+        int notTake = max(left.first, left.second) + max(right.first, right.second);
+
+        return {take, notTake};
+    }
+public:
+    int rob(TreeNode* root) {
+        pair<int, int> p = helper(root);
+        return max(p.first, p.second);
+    }
+};
