96eec5fa784389f0f3d4b6e1c27c9f1307252ce1
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-04-09 12:50:58 +0000

    [LeetCode Sync] Runtime - 7 ms (18.67%), Memory - 9.7 MB (50.12%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int indexArr1[26] = {0};
-        int indexArr2[26] = {0};
-        
-        //if s and t have different lenght then there is no chance of Anagaram
-        if(s.length() != t.length()){
-            return  false;
+        if(s.size() != t.size()) return false;
+        map<char, int> mpp;
+        for(char ch : s){
+            mpp[ch]++;
         }
-        
-        // marking every character occurence of 1st sting 
-        for(int i=0; i< s.length(); i++){
-            indexArr1[s[i]-97]++;
-                     
-        }
-        
-        // marking every character occurence of 2nd sting  
-        for(int i=0; i< t.length(); i++){       
-            indexArr2[t[i]-97]++;           
-        }
-        
-        
-        // here no of unique characters of string 1 and string 2 is same of check of Anagram
-        for(int i=0; i<26; i++){
-            if( indexArr1[i] != indexArr2[i]){
-                return false;
-            }
+
+        for(char ch: t){
+            if(mpp[ch]) mpp[ch]--;
+            else return false;
         }
         return true;
+
     }
 };
