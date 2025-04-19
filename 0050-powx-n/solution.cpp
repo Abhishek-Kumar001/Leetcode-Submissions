9b9ca0c76c2a886779eaa6c4623b987e6d034622
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-04-19 14:02:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (71.12%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,21 +1,33 @@
 class Solution {
-public:
-    void pow( double x, int n, double &ans){
-        if( n == 0) return ;
-        
-        if(n&1){
-            ans = ans*x;
+    double helper(double x,  long long n, double ans){
+        // base case 
+        if(n == 0) {
+            return ans;
         }
-        pow(x*x, n/2, ans);
-    }
-    double myPow(double x, int n) {
-        // STRIVER APP
-        double ans = 1.0;
-        long nn=n;
-        if( nn<0) nn= -1*nn;
+
+        if(n % 2 == 1){
+            ans *= x;
+        }
+
+        x*=x;
+        n = n/2;
+        ans = helper(x, n, ans);
         
-        pow(x, nn, ans);
-        if( n <0) ans = (double) 1.0 / ans;
         return ans;
     }
+public:
+    double myPow(double x, int n) {
+       double ans = 1;
+       long long nn = n;
+       nn = abs(nn);
+       ans = helper(x, nn, ans);
+
+       if(n < 0) {
+        return 1/ ans;
+       }
+       return ans;
+    }
 };
+
+
+
