8be931d1c7306a1b82c923d3aed16ad9639d001a
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-05-18 07:12:14 +0000

    [LeetCode Sync] Runtime - 84 ms (2.55%), Memory - 9.3 MB (21.42%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,52 +1,70 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        // remove the starting space
+
+        // finding the 1st ans-space index i.e igansring the white space
         int index = 0;
         for(int i=0; i<s.length(); i++){
-            if(s[i] == ' '){
-                index++;
-                continue;
-            }else break;
+            if(s[i] != ' '){
+                index = i;
+                break;
+            }
         }
-        cout<<"starting index = "<<index<<endl;
+        cout<<index;
 
+        // checking after removind the white-spaces is the 1st letter is ('-' or '+') or anst
+        bool isNegative = false;
+        if(s[index] == '-'){
+            isNegative = true;
+            cout<<"negative found"<<endl;
+            index++;  // vvvvvi
+        } 
+        else if(s[index] == '+') index++;  // vvvvvvi
+
+
+
+        // now from here if found number then add it otherwise break and return ans;
         int ans = 0;
-        bool negativeOrNot = false;
-        bool foundDigitAlready = false;
-        bool minusOrPlusFoundAlready = false;
+        cout<<"index = "<<index<<endl;
         for(int i=index; i<s.length(); i++){
-            if(s[i] == ' ' )return negativeOrNot ? -ans: ans;  // we have found space after char
+            int currans = s[i]-48;
+           
+            cout<<"index= "<<i<<"  ans="<<ans;
+            if(currans  >= 0  && currans <=9){
+                // if(ans <= INT_MAX/10)
+                //    if(ans == INT_MAX/10){
+                //         if( currans <= 7) ans = (ans*10) + (currans);
+                //         else{
+                //             if(isNegative) return INT_MIN;
+                //             else return INT_MAX;
+                //         }
+                //    }else{
+                //         ans = (ans*10) + (currans);
+                //    }
+                  
+                // else {
+                //     // i thing this else code will never execute b/c if ans exceed int range we 
+                //     // return from above if block
+                //
+                //     if(isNegative) return INT_MIN;
+                //     else return INT_MAX;
+                // }
 
-            if(s[i] == '-' || s[i] == '+'){
-                if(minusOrPlusFoundAlready) return negativeOrNot ? -ans: ans;
 
-                minusOrPlusFoundAlready = true;
-                if(foundDigitAlready){
-                    cout<<s[i]<<" found and foundDigitAlready is also true so returned";
-                    return negativeOrNot ? -ans: ans;
-
-                }
-                if(s[i] == '-')
-                  negativeOrNot = true;
-            } 
-            else if(s[i] > 58 || s[i] < 48){
-                cout<<"non-negative ingerger found "<<s[i];
-                return  negativeOrNot ? -ans: ans;
- 
-            } 
-            else{
-                foundDigitAlready = true;
-                if(ans > INT_MAX/10) return negativeOrNot ? INT_MIN : INT_MAX;
+                
+                // THIS METHOD IS BETTER THAN ABOVE ONE
+                if(ans > INT_MAX/10) return isNegative ? INT_MIN : INT_MAX;
 
                 ans*=10;
-                if((INT_MAX - ans) < (s[i]-48))  return negativeOrNot ? INT_MIN : INT_MAX;
+                if((INT_MAX - ans) < (s[i]-48))  return isNegative ? INT_MIN : INT_MAX;
                 ans += (s[i] - 48);
                 cout<<s[i]<<"  "<<ans<<endl;
             }
+            else break;
+
+            cout<<"index= "<<i<<"  ans="<<ans;
         }
-        cout<<"return at the end";
-        return  negativeOrNot ? -ans: ans;
-        
+        return isNegative? -ans: ans;
+
     }
 };
