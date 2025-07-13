25a2cc074e7f944bb21dafbb231af4961795269f
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-07-13 08:52:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.2 MB (79.34%)

--- 0088-merge-sorted-array/solution.cpp
+++ 0088-merge-sorted-array/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1, j=0;
-        while(i>=0 && j<n){
-            if(nums1[i] > nums2[j]){
-                swap(nums1[i], nums2[j]);
-                i--; j++;
-            }else break;
+        // bruit force
+        // TC - O(klogk); where k = m+n;
+        // SC - O(0)
+
+        // put all the elements of nums2 in nums1 and sort the nums1;
+        int j=0;
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[j++];
         }
 
-        sort(nums1.begin(), nums1.begin()+m);
-        sort(nums2.begin(), nums2.end());
-
-        for(int i=0; i<n; i++){
-            nums1[m++] = nums2[i];
-        }
+        // sorting takes nlogn  where n is the totel size of array
+        sort(nums1.begin(), nums1.end());
     }
 };
