e642fcd6eaf24e434a89105f9e789060647adffa
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-07-17 18:21:42 +0000

    [LeetCode Sync] Runtime - 2 ms (4.31%), Memory - 19.5 MB (28.33%)

--- 0021-merge-two-sorted-lists/solution.cpp
+++ 0021-merge-two-sorted-lists/solution.cpp
@@ -11,24 +11,31 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ansList = new ListNode(10);
-        ListNode* ansHead = ansList;
-        ListNode* curr1 = list1;
-        ListNode* curr2 = list2;
-        
-        while(curr1 != NULL  && curr2 != NULL){
-            if(curr1->val <= curr2->val){
-                ansList->next = curr1;
-                ansList = ansList->next;
-                curr1 = curr1->next;
+        // create a ans link list with a dummy node
+        ListNode* ansHead = new ListNode(-1);
+        ListNode* ansTemp = ansHead;
+
+        while(list1 != NULL  && list2 != NULL){
+            if(list1->val < list2->val){
+                ansTemp->next = list1;
+                list1 = list1->next;
             }else{
-                ansList->next = curr2;
-                ansList = ansList->next;
-                curr2 = curr2->next;
+                ansTemp->next = list2;
+                list2 = list2->next;
             }
+            ansTemp = ansTemp->next;
         }
-        ansList->next = curr1 == NULL ? curr2 : curr1;
-        
+        while(list1 != NULL){
+            ansTemp->next = list1;
+            ansTemp = ansTemp->next;
+            list1 = list1->next;
+        }
+        while(list2 != NULL){
+            ansTemp->next = list2;
+            ansTemp = ansTemp->next;
+            list2 = list2->next;
+        }
+
         return ansHead->next;
     }
 };
