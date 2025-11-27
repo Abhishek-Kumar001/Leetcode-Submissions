4204ed5a6301dc8d7156b041718bc30c14362fe3
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-11-27 15:41:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 59.4 MB (59.01%)

---
+++ 0784-insert-into-a-binary-search-tree/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* helper(TreeNode* root, int val){
+        // base case
+        if(root == NULL){
+            TreeNode* newNode = new TreeNode(val);
+            return newNode;
+        }
+
+        if(root->val < val) root->right = helper(root->right, val);
+        else root->left = helper(root->left, val);
+
+        return root;
+    }
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        return helper(root, val);
+    }
+};
