d67079e8e99de8752ced202598e5c94ddeafde7e
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-11-30 16:40:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (24.22%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,103 +1,18 @@
-     // Bruit Force - recursive way
-
-// class Solution {
-//     int helper(int ind, vector<int> nums){
-//         // base case
-//         if(ind >= nums.size()) return 0;
-
-//         // choice 1 - take it 
-//         int take = nums[ind] + helper(ind+2, nums);
-
-//         //choice 2 - not take it
-//         int notTake =  0 + helper(ind+1, nums);
-
-//         return max(take, notTake);
-//     }
-// public:
-//     int rob(vector<int>& nums) {
-//         return helper(0, nums);
-//     }
-// };
-
-
-
-
-
-
-
-
-//             // memoization
-// class Solution {
-//     int helper(int ind, vector<int> nums, vector<int> &dp){
-//         // base case
-//         if(ind >= nums.size()) return 0;
-
-//         // memoize it
-//         if(dp[ind] != -1) return dp[ind];
-
-//         // choice 1 - take it 
-//         int take = nums[ind] + helper(ind+2, nums, dp);
-
-//         //choice 2 - not take it
-//         int notTake =  0 + helper(ind+1, nums, dp);
- 
-//         return dp[ind] =  max(take, notTake);
-//     }
-// public:
-//     int rob(vector<int>& nums) {
-//         vector<int> dp(nums.size(), -1);
-//         return helper(0, nums, dp);
-//     }
-// };
-
-
-
-
-          // tabulation
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
 
-        vector<int> dp(nums.size(), -1);
+        vector<int> dp(nums.size());
         dp[0] = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            int take = INT_MIN;
-            if( i > 1) take = nums[i] + dp[i-2];
+        dp[1] = max(nums[0], nums[1]);
 
-            int notTake = INT_MIN;
-            if( i > 0) notTake = max(dp[i-1], nums[i]);
-
-            dp[i] = max(take, notTake);
+        for(int i=2; i<nums.size(); i++){
+            int take = nums[i] + dp[i-2];
+            int notTake = dp[i-1];
+            dp[i] = max(take , notTake);
         }
-        // int maxi = dp[0];
-        // for(int i=0; i<dp.size(); i++) maxi = max(maxi, dp[i]);
-        // return maxi;
 
         return dp[nums.size()-1];
     }
 };
-
-
-
-
-//        // space optimization
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         // base case -- vvi
-//         if(nums.size() == 1) return nums[0];
-
-//         int prev1 = nums[0];
-//         int prev2 = max(nums[0], nums[1]);
-
-//         for(int i=2; i<nums.size(); i++){
-//             int take = nums[i] + prev1;
-//             int notTake = prev2;
-
-//             prev1 = prev2;
-//             prev2 = max(take, notTake);
-//         }
-
-//         return prev2;
-//     }
-// };
