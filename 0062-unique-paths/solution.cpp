ac86aad9391e4df291e4bcc37021e8f60671124d
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-01 16:20:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (30.77%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,51 +1,18 @@
-//               // RECURSION
-// class Solution {
-//     int helper(int r, int c, int m, int n){
-//         // base case 
-//         if(r == m-1  && c == n-1) return 1;
-
-//         if(r >=m || c>=n) return 0;
-
-//         // if valid go down
-//         int down = helper(r+1, c, m, n);
-
-//         // if valid go right
-//         int right = helper(r, c+1, m, n);
-
-//         return down + right;
-//     }
-// public:
-//     int uniquePaths(int m, int n) {
-//         return helper(0, 0, m, n);
-//     }
-// };
-
-
-
-
-              // MEMOIZATION
 class Solution {
-    int helper(int r, int c, int m, int n, vector<vector<int>> &dp){
-        // base case 
-        if(r == m-1  && c == n-1) return 1;
-
-        if(r >=m || c>=n) return 0;
-
-        // MOMOIZE IT
-        if(dp[r][c] != -1) return dp[r][c];
-
-
-        // if valid go down
-        int down = helper(r+1, c, m, n, dp);
-
-        // if valid go right
-        int right = helper(r, c+1, m, n, dp);
-
-        return dp[r][c] = down + right;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(0, 0, m, n, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 || j == 0) dp[i][j] = 1;
+                else{
+                    int up = dp[i-1][j];
+                    int left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
