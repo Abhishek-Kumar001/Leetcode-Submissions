b2ece1610172c0899e56e967f4f2a148a98e2023
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-01 17:04:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16 MB (74.06%)

--- 0064-minimum-path-sum/solution.cpp
+++ 0064-minimum-path-sum/solution.cpp
@@ -1,58 +1,20 @@
-// class Solution {
-// public:
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int n=grid.size(), m=grid[0].size();
-
-//         vector<int> prev(m,0);
-//         prev[0] = grid[0][0];
-//         for(int j=1; j<m; j++){
-//             prev[j] = prev[j-1] + grid[0][j];
-//         }
-        
-//         for(int i=1; i<n; i++){
-//             vector<int> curr(m, 0);
-//             for(int j=0;j<m; j++){
-//                 if(j==0){
-//                     curr[j] =  grid[i][j] + prev[j];
-//                 }
-//                 else{
-//                     curr[j] =  grid[i][j] + min(curr[j-1] , prev[j]);
-//                 }
-//             }
-//             prev = curr;
-//         }
-//         return prev[m-1];
-//     }
-// };
-
-
-
-
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> prev(n,0);
 
-        vector<int> prev(m, 1e8);
-        
-        for(int i=0; i<n; i++){
-            vector<int> curr(m, 0);
-            for(int j=0; j<m; j++){
-                if(i==0  && j==0){
-                    curr[0] = grid[0][0];
-                   
-                }else{
-                    int left = 1e9;
-                    if(j != 0) left = curr[j-1];
-                    int up = 1e9;
-                    if(i != 0) up = prev[j];
-
-                    curr[j] = grid[i][j] + min(left, up); 
-                }                
+        for(int i=0; i<m; i++){
+            vector<int> curr(n,0);
+            for(int j=0; j<n; j++){
+                if(i == 0 && j == 0) curr[0] = grid[0][0];
+                else if( i == 0 ) curr[j] = grid[i][j] + curr[j-1];
+                else if( j == 0 ) curr[j] = grid[i][j] + prev[j];
+                else curr[j] = grid[i][j] + min(prev[j], curr[j-1]);
             }
             prev = curr;
         }
-
-        return prev[m-1];
+        return prev[n-1];
     }
 };
