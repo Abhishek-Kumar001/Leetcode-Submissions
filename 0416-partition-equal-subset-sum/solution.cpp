51ac65a250e3aaf76e467c72de2b3036015a6e85
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-02 16:30:53 +0000

    [LeetCode Sync] Runtime - 151 ms (35.95%), Memory - 110.5 MB (32.22%)

--- 0416-partition-equal-subset-sum/solution.cpp
+++ 0416-partition-equal-subset-sum/solution.cpp
@@ -1,41 +1,26 @@
 class Solution {
-    bool subsetSumTarget(vector<int> &nums, int target){
-        int n= nums.size();
-        vector<int> prev(target+1, 0), curr(target+1, 0);
-        // base case-1
-        prev[0]= 1;
-        //base case vvi
-        if(nums[0] <= target){
-            prev[nums[0]]= 1;
-        }
-
-        for(int i=1; i<n; i++){
-            for(int tar=1; tar<=target; tar++){
-                // take
-                int take = 0;
-                if( nums[i] <= tar){
-                    take  = prev[tar-nums[i]];
-                }
-                //not take 
-                int notTake = prev[tar];
-        
-                curr[tar] = take || notTake;
-            }
-            prev = curr;
-        }
-        return prev[target];
-    }
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(auto it: nums){
-            sum+=it;
-        }
-
+        int sum = 0;
+        for(int val : nums) sum+=val;
         if(sum % 2 == 1) return false;
 
         int target = sum / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target+1, 0));
 
-        return subsetSumTarget(nums, target);  
+        for(int i=0; i<n; i++) dp[i][0] = 1;
+        if(nums[0] <= target) dp[0][nums[0]] = 1;
+        
+        for(int i=1; i<n; i++){
+            for(int tar = 1; tar<=target; tar++){
+                int take = 0;
+                if(nums[i] <= tar ) take = dp[i-1][tar-nums[i]];
+                int notTake = dp[i-1][tar];
+
+                dp[i][tar] = take || notTake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
