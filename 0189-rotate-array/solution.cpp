d2c840cf315c735dbc6472ff59269a9798ee041b
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-05 18:26:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.4 MB (99.82%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k%nums.size();
-        vector<int> temp(k);
-        int j=0;
-        for( int i=nums.size()-k; i<nums.size(); i++){
-             temp[j++] = nums[i];
-        }
-        // now shift the all n-k elements to the left 
-        for( int i=nums.size()-k-1; i>=0; i--){
-           nums[i+k] = nums[i];
-        }
-        //now put all k extracted elements to the starting
-        for( int i=0; i<k; i++){
-           nums[i] = temp[i];
-        }
+        k = k % nums.size();
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
         
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Reverse the rest of the elements after k
+        reverse(nums.begin() + k, nums.end());
     }
 };
