066a8c4e6ef1097e2971bcac896d65e862fb8770
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-07 16:21:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (99.98%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        // Love babber approach
-        int n = nums.size();
-        int cnt = 0;
-        for(int i=1; i<n; i++){
-            if( nums[i-1]  > nums[i] ){
+        int j = -1;
+        int cnt = 0;;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] < nums[i-1]){
                 cnt++;
             }
         }
-        
-        if( nums[n-1] > nums[0]){
-            cnt++;
-        }
-        
-        return cnt<=1;
+        if(nums[0] < nums[nums.size()-1]) cnt++;
+
+        return cnt <= 1 ;
     }
 };
