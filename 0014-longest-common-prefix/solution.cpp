3d87b9def4eb29563537ce56f8a52f5c01722c76
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-12 15:22:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.7 MB (79.44%)

--- 0014-longest-common-prefix/solution.cpp
+++ 0014-longest-common-prefix/solution.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
-        /*
-        // corner case for if vector have only one string 
-        if(strs.size() == 1){
-            return strs[0];
-        }
-        */
-
-        string ans="";
-        int n= strs.size();
-
-      
-        int i=0;
-        while( true ){
-            int flag =1;
-            // // if we are not handling corner case above then run this loop from j=0 so that
-            // we can also check that i can't exceed from its strs[0] length other wise 
-            // hmara i badhthta jayega and stack overflow aayega b/c hm 
-            // i ko iss for loop le andar check kar rhe hai aur hm iss loop me kabhi ja hi nahi rahe
-            // jab vector ki size 1 hai 
-
-            for(int j=0; j<n; j++){  // note j ko hm 0 se chalaye hai yahi , it is always the loop 
-                                     // from j=0;    
-                if( i>= strs[0].length() ||  i >= strs[j].length() ||  strs[j][i] != strs[0][i]){
-                    flag = 0;
+        string prefix = "";
+        int flag = 0;
+        for(int i=0; i<strs[0].size(); i++){
+            for(int j=1; j<strs.size(); j++){
+                if(strs[j].size() >= i+1){
+                    if(strs[0][i] != strs[j][i]){ 
+                        flag = 1;
+                        break;
+                    }
+                }
+                else{
+                    flag = 1;
+                    break;
                 }
             }
-            if(flag == 1){
-                ans.push_back(strs[0][i]);
-                
-                i++;
-            }
-            else{
-                i++;
-                break;
-            }
-            
+            if(flag == 1) break;
+            prefix+=strs[0][i];
         }
-        
-        return ans;
-       
+        return prefix;
     }
 };
