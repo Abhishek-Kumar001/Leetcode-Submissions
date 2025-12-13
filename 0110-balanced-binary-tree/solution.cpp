9e12c2cd5ca15b508b56e94a83080ed94f13a56d
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-13 18:35:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.1 MB (56.52%)

--- 0110-balanced-binary-tree/solution.cpp
+++ 0110-balanced-binary-tree/solution.cpp
@@ -9,51 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-    int helper(TreeNode* root){
+    int height( TreeNode* root){
         if(root == NULL) return 0;
 
-        int left =  helper(root->left);
-        int right =  helper(root->right);
-        
-        if(left == -1 || right == -1) return -1;
+        int leftHeight = height(root->left);
+        if(leftHeight == -1) return -1;
+        int rightHeight = height(root->right);
+        if(rightHeight == -1) return -1;
 
-        if(abs(left - right) > 1) return -1;
-        return max(left , right) + 1;
+        if(abs(leftHeight - rightHeight) > 1 ) return -1;
+
+        return max(leftHeight, rightHeight) + 1;
     }
 public:
     bool isBalanced(TreeNode* root) {
-        int result =  helper(root);
-        if(result == -1) return false;
-        return true;
+        // we will use the height function and in that we calculat height as well as the 
+        // it is balanced or not
+
+        return  height(root) != -1;
     }
 };
-
-
-
-// class Solution {
-//     int height(TreeNode* root){
-//         if(root == NULL) return 0;
-
-//         int left = 1 + height(root->left);
-//         int right = 1 + height(root->right);
-        
-//         return max(left , right);
-//     }
-// public:
-//     bool isBalanced(TreeNode* root) {
-//         if(root == NULL) return true;
-
-//         bool left = isBalanced(root->left);
-//         bool right = isBalanced(root->right);
-
-//         if(!left || !right) return false;
-
-//         int leftHeight = height(root->left);
-//         int rightHeight = height(root->right);
-
-//         if(abs(leftHeight - rightHeight) > 1) return false;
-//         else return true;
-//     }
-// };
