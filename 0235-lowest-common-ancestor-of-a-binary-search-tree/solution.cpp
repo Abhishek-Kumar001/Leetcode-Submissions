79c814be189d64deec0a2497e282478e7210f610
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-13 18:16:25 +0000

    [LeetCode Sync] Runtime - 29 ms (12.61%), Memory - 23.5 MB (66.67%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
@@ -7,49 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
 
-          // my code // 
-// class Solution {
-// public:
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         //base case
-//         if(root == NULL ) return NULL;
-      
-//         if(root->val == p->val || root->val == q->val) return root;
-
-//         TreeNode* left = NULL;
-//         if( root->val > p->val || root->val > q->val ){
-//             left = lowestCommonAncestor(root->left, p, q);
-//         }
-        
-//         TreeNode* right = NULL;
-//         if(root->val < p->val || root->val < q->val){
-//             right = lowestCommonAncestor(root->right, p, q);
-//         }
-
-//         if(left != NULL && right != NULL) return root;
-//         if(left != NULL) return left;
-//         if(right != NULL) return right;
-//         return NULL;
-//     }
-// };
-
-                // striver codee
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //base case
-        if(root == NULL ) return NULL;
-      
-        if( root->val > p->val && root->val > q->val ){
-            return lowestCommonAncestor(root->left, p, q);
-        }
-       
-        if(root->val < p->val && root->val < q->val){
-            return  lowestCommonAncestor(root->right, p, q);
-        }
-        
-        return root;
+        // base base 
+        if(!root) return NULL;
+
+        // base case 2 
+        if(root->val == p->val || root->val == q->val) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if( left && right) return root;
+        else if(left) return left;
+        else if(right) return right;
+        return NULL;
     }
 };
