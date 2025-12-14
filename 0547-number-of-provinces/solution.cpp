01c917910909b3c7d50f95e899e8aa6f1948f5d1
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2025-12-14 18:30:38 +0000

    [LeetCode Sync] Runtime - 11 ms (5.14%), Memory - 24.9 MB (5.48%)

--- 0547-number-of-provinces/solution.cpp
+++ 0547-number-of-provinces/solution.cpp
@@ -1,35 +1,40 @@
 class Solution {
-    void dfs(vector<vector<int>>& isConnected, int node, vector<int> &visited){
+    void dfs(int node, vector<int> &visited, unordered_map<int, vector<int>> mpp){
         queue<int> q;
         q.push(node);
 
         while(!q.empty()){
             int node = q.front();
             q.pop();
-
-            for(int i=0; i<isConnected[node].size(); i++){
-                if(isConnected[node][i] && visited[i] == 0){
-                    q.push(i);
-                    visited[i] = 1;
+            for(auto neigh : mpp[node]){
+                if(visited[neigh] == 0){
+                    q.push(neigh);
+                    visited[neigh] = 1;
                 }
             }
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // without creating adjlist, uisng index as nodes 
         int n = isConnected.size();
-        vector<int> visited(n, 0);
-
-        int noOfProvinces = 0;
+        unordered_map<int, vector<int>> mpp;
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                visited[i] = 1;
-                dfs(isConnected, i, visited);
-                noOfProvinces++;
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j]) mpp[i].push_back(j);
             }
         }
 
-        return noOfProvinces;
+
+        // do dfs
+        int cnt = 0;
+        vector<int> visited(n, 0);
+        for(int i=0; i<n; i++){
+            if(visited[i] == 0){
+                cnt++;
+                dfs(i, visited, mpp);
+            }
+        }
+
+        return cnt;
     }
 };
