693ae683d59f43f2a6c5b17acce971f3d6157869
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-02-10 17:05:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.5 MB (82.50%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,67 +1,20 @@
-// class Solution {
-// public:
-//     bool isWithoutRepeating(string s){
-//         map<char, int> mpp;
-
-//         for(char ch : s){
-//             if(mpp[ch] != 0) return false;
-//             mpp[ch]++;
-//         }
-//         return true;
-//     }
-
-//     int lengthOfLongestSubstring(string s) {
-//         // M1 - Bruit force (we will generate all the substrings and check for 
-//         // longest substring without repeating characters)
-
-//         // TLE
-//         // TC - O(n^2 * n)
-//         // SC = O(n^2 * n);
-        
-//         int maxLength = 0;
-//         for(int i=0; i<s.size(); i++){
-//             string substr = "";
-//             for(int j=i; j<s.size(); j++){
-//                 substr+=s[j];
-
-//                 if(isWithoutRepeating(substr)){
-//                     int currSize = substr.size();
-//                     maxLength = max(maxLength, currSize);
-//                 }
-//             }
-//         }
-
-//         return maxLength;
-//     }
-// };
-
-
-
-
-                      // good 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // M2 optimized
-        // sliding window app (apna clg)
-
-        // TLE
-        // TC - O(n)
-        // SC = O(0);
-        
-        vector<int> charIndex(256, -1);
-        int start = 0, maxLen = 0;
+        int start = 0;
+        vector<int> track(256, -1);
+        int maxi = 0;
         for(int i=0; i<s.size(); i++){
-            int lastPosi = charIndex[ s[i] ];
-            if( lastPosi >= start){
-                start = lastPosi+1;
+            char ch = s[i];
+            if(track[ch] != -1){  // it means this char is already present
+                // start = track[ch]+1;  // wrong : You must never move start backward. eg: abba
+                start = max(start, track[ch] + 1);
+
             }
-            maxLen = max(maxLen, i-start+1);
-            charIndex[s[i]] = i;
+            maxi = max(maxi, i-start+1);
+            track[ch] = i;
         }
 
-        return maxLen;
+        return maxi;
     }
 };
-
-
