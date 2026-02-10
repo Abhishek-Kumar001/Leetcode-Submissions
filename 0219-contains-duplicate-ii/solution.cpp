df305a8d1ed9f254cb08f90216fce687ad14da1c
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-02-10 17:40:08 +0000

    [LeetCode Sync] Runtime - 155 ms (12.29%), Memory - 102.3 MB (21.63%)

--- 0219-contains-duplicate-ii/solution.cpp
+++ 0219-contains-duplicate-ii/solution.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int,int>mpp;
-        for(int i=0; i<nums.size(); i++){
-            // if(mpp.find(nums[i]) != mpp.end()){
-            //     int index=mpp[nums[i]];
-            //     int dif=abs(index-i);
-            //     if(dif <= k)
-            //     return true;
-            // }
+        map<int, int> mpp;
 
-            if(mpp.count(nums[i])){
-                int index=mpp[nums[i]];
-                int dif=abs(index-i);
-                if(dif <= k)
-                return true;
+        for(int i=0; i<nums.size(); i++){
+            // if(mpp[nums[i]]){  
+                // don't use this becuase if it not found then it initialized with 0
+                // it means that number is not present i.e for eg1, 1 is not present as 1 is inserted as 0
+        
+            if (mpp.find(nums[i]) != mpp.end()) {
+                int diff = abs(i - mpp[nums[i]]);
+                if(diff <= k) return true;
             }
-            mpp[nums[i]]=i;
+            mpp[nums[i]] = i;
         }
         return false;
     }
