2634df082b2d9c98da76b334ef57983f8690fb87
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-02-12 18:18:02 +0000

    [LeetCode Sync] Runtime - 1246 ms (5.04%), Memory - 177.4 MB (5.92%)

--- 0220-contains-duplicate-iii/solution.cpp
+++ 0220-contains-duplicate-iii/solution.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// public:
+//     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+//         // bruit force 
+
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(abs(i-j) <= indexDiff){
+//                     if(abs(nums[i] - nums[j]) <= valueDiff){
+//                         return true;
+//                     }
+//                 }else break;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
@@ -6,13 +26,14 @@ public:
         set<int> ss(nums.begin(), nums.end());
         if(valueDiff == 0 && n == ss.size()) return false;
 
-        
-        for(int i=0; i<n; i++){          
-            for(int j=i+1; j<=i+indexDiff && j<n; j++ ){
-                if(abs(nums[i] - nums[j])  <= valueDiff){
-                    return true;
-                }
-            }        
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(abs(i-j) <= indexDiff){
+                    if(abs(nums[i] - nums[j]) <= valueDiff){
+                        return true;
+                    }
+                }else break;
+            }
         }
         return false;
     }
