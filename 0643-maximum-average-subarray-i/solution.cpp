6289fa01b0542f9ceba2b17b13b37614d726fe41
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-02-18 17:03:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 113.6 MB (87.20%)

--- 0643-maximum-average-subarray-i/solution.cpp
+++ 0643-maximum-average-subarray-i/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double sum = 0;
-       // double maxAvg = 0; // vvvi -  0 se initialize na kare
-        double maxAvg = -DBL_MAX;  // vvvi 
-        // there is no DBL_MIN fun in c++ only DBL_MAX fun is avilable 
-       
-        for(int i=0; i<nums.size(); i++){
-            if(i < k){
-                sum+=nums[i];
-                if(i+1 == k)
-                   maxAvg = max(maxAvg, sum/k);
-            }
-            else{
-                sum-=nums[i-k];
-                sum+=nums[i];
-                maxAvg = max(maxAvg, sum/k);
-            }
+        double temp = 0;
+        double ans = 0;
+
+        for(int i=0; i<k; i++){
+            temp+=nums[i];
         }
-        return maxAvg;
+
+        ans = temp / k;
+
+        for(int i=k; i<nums.size(); i++){
+            temp-= nums[i-k];
+            temp+= nums[i];
+
+            ans = max( ans, temp/k);
+        }
+
+        return ans;
     }
 };
