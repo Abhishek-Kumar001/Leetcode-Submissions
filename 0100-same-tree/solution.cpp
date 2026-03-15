f20afc70f77d82244b039738cc879a7bd87bfbfc
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-03-15 17:27:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.8 MB (48.51%)

--- 0100-same-tree/solution.cpp
+++ 0100-same-tree/solution.cpp
@@ -13,14 +13,12 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
-        if(p == NULL && q != NULL) return false;
-        if(p != NULL && q == NULL) return false;
+        if(p == NULL || q == NULL) return false;
 
         if(p->val != q->val) return false;
 
         bool left = isSameTree(p->left, q->left);
-        if(!left) return false;
-        bool right = isSameTree(p->right, q->right);
-        return right;
+        if(left == false) return false;
+        return isSameTree(p->right, q->right);
     }
 };
