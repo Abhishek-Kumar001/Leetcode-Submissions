6717057937dde943bd34e8998445b767e3fde302
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-03-20 16:34:25 +0000

    [LeetCode Sync] Runtime - 2 ms (88.91%), Memory - 9.9 MB (45.12%)

--- 0038-count-and-say/solution.cpp
+++ 0038-count-and-say/solution.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     string countAndSay(int n) {
         if(n == 1) return "1";
-        if(n == 2) return "11";
-
-        string tempAns = "11";
-        for(int i=3; i<=n; i++){
-            int cnt = 1;   // vvi not cnt = 0
-            string s = "";
-            for(int j=0; j<tempAns.size(); j++){
-                if(j != tempAns.size()-1  && tempAns[j] == tempAns[j+1]){
-                    cnt++;
-                }else{
-                   s+=(cnt + 48);
-                   s+=tempAns[j];
-
-                   cnt = 1;  // vvi not cnt = 0;
+        
+        string ans = "1";
+        for(int i=2; i<=n; i++){
+            char last = ans[0];
+            int count = 1;
+            string res = "";
+            
+            for(int j=1; j<ans.length(); j++){
+                if(ans[j] == last) count++;
+                else{
+                    res += to_string(count) + last;
+                    last = ans[j];
+                    count =1;
                 }
             }
-            tempAns = s;
+            res += to_string(count) + last;
+            ans = res;
         }
 
-        return tempAns;
+        return ans;
     }
 };
