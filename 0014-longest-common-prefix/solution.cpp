cce41f32fbf79d20fdf2639c407a28de298d7908
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-03-20 05:12:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.7 MB (79.44%)

--- 0014-longest-common-prefix/solution.cpp
+++ 0014-longest-common-prefix/solution.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = "";
-        int flag = 0;
-        for(int i=0; i<strs[0].size(); i++){
-            for(int j=1; j<strs.size(); j++){
-                if(strs[j].size() >= i+1){
-                    if(strs[0][i] != strs[j][i]){ 
-                        flag = 1;
-                        break;
-                    }
-                }
-                else{
-                    flag = 1;
-                    break;
+
+        for(int i=0; i<strs[0].length(); i++){
+            for(int j = 1; j<strs.size(); j++){
+                if(strs[j].length() >= i+1){
+                   if(strs[0][i] != strs[j][i]) return prefix;
+                }else{
+                    return prefix;
                 }
             }
-            if(flag == 1) break;
-            prefix+=strs[0][i];
+            prefix += strs[0][i];
         }
         return prefix;
     }
