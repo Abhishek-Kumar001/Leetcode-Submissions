e9055ee18ebec94de9c3c98882df08b182050242
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-05 14:59:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.6 MB (96.25%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -1,33 +1,28 @@
-// @tech dose mathod
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int k = -1, n = nums.size();
+        //
+        int n = nums.size();
+        int ind = -1;
         for(int i=n-1; i>0; i--){
-            if( nums[i-1] < nums[i]){
-                k = i-1;
+            if(nums[i-1] < nums[i]){
+                ind = i-1;
                 break;
             }
         }
-
-        // it means arrays is in descending order in this case next permutation is 
-        // reversed(or  starting,  or sorted array) a/c question
-        if(k == -1){
-            reverse(nums.begin(), nums.end());
-            return ;
+        if(ind == -1){
+           reverse(nums.begin(), nums.end());
+           return;
         }
 
-        // next permuatation need to found
-        int replaceIndex = k+1;
-        for(int i=k+2; i< n; i++){
-            if( nums[i] > nums[k] && nums[i] < nums[replaceIndex] ){
-                replaceIndex = i;
-            }
+        int k = ind+1;
+        for(int i=k+1; i<n; i++){
+            if(nums[i] > nums[ind] && nums[i] < nums[k]) k = i;
         }
-        
-        swap(nums[k], nums[replaceIndex]);
-        cout<< nums[k]<< "   "<< nums[replaceIndex];
-        sort(nums.begin()+(k+1), nums.end());
+
+        swap(nums[ind], nums[k]);
+        sort(nums.begin()+ind+1, nums.end());
+
+        return;
     }
 };
