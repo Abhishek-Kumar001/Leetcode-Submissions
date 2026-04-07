d531bbc62e9e1a2b5e6f2c3c473fdf40f90416b2
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-07 14:58:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.5 MB (88.40%)

--- 0066-plus-one/solution.cpp
+++ 0066-plus-one/solution.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& v) {
-        int n = v.size();
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
         int carry = 1;
-        for(int i = n-1; i >= 0; i--){
-           int sum = carry + v[i];
-           carry = sum / 10;
-           sum = sum % 10;          
-           v[i] = sum;
+        for(int i=n-1; i>=0; i--){
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            if(sum == 10) carry = 1;
+            else carry = 0;
         }
-        if( carry == 1){
-            vector<int> newVec(n+1, 0);
-            newVec[0] = 1;          
-            return newVec;
+
+        if(carry == 1){
+            vector<int> ans(n+1);
+            ans[0] = 1;
+            for(int i=1; i<=n; i++){
+                ans[i] = digits[i-1];
+            }
+            return ans;
         }
-        return v;
+
+        return digits;
     }
 };
