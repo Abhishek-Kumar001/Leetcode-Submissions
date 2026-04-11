3302f3506ca2a1ca5519ebeb56c9cc015528ff58
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-11 13:22:23 +0000

    [LeetCode Sync] Runtime - 2 ms (38.13%), Memory - 14 MB (11.05%)

--- 0137-single-number-ii/solution.cpp
+++ 0137-single-number-ii/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int prev = nums[0];
-        if (nums.size() == 1)
-            return nums[0];
+        unordered_map<int, int> mpp;
+        
+        for(int x : nums) mpp[x]++;
 
-        for (int i = 2; i < nums.size(); i += 2) {
-            if (nums[i] != prev)
-                return prev; // it means the prev no. don't has 3 occerence
-            // else  this no has 3 occerence;
-            //  so check for next no. and next no. is:-  nums[i+1]
-            prev = nums[i + 1];
-            i++;
+        for(auto it : mpp){
+            if(it.second == 1) return it.first;
         }
-        return prev;
+
+        return -1;
     }
 };
