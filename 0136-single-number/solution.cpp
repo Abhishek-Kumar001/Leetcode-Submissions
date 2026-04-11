d2c4f307a5c0df7ac02d63dbaf57d46f3464f3fb
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-11 13:20:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (24.40%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int sum = 0;
-        for(int i=0; i<nums.size(); i++){
-             sum^=nums[i];
-        }
-        return sum;
+        int result = 0;
+        for(int x : nums) result ^= x;
+
+        return result;
     }
 };
