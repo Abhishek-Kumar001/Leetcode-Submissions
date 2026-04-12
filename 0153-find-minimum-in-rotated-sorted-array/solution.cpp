822a6689529b9702c06f70a7f2d4b0cf85696c3c
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-12 13:50:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.1 MB (50.85%)

--- 0153-find-minimum-in-rotated-sorted-array/solution.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // @striver code
+        int s = 0, e = nums.size()-1;
+
         int mini = INT_MAX;
+        while(s <= e){
+            int mid = (e+s)/2;
 
-        int low =0;
-        int high = nums.size()-1;
-
-        while( low <= high){
-            int mid = (low+high)/2;
-
-            if( nums[low] > nums[mid]){
-                // means left part is sorted
-                // so updata mini ans move to left
-                mini = min( mini, nums[mid]);
-                high = mid-1;
-            }
-            else{
-                mini = min( mini, nums[low]);              
-                low = mid+1;
+            if(nums[s] <= nums[mid]){
+                //means 1st half is sorted;
+                mini = min(mini, nums[s]);
+                s = mid+1;
+            }else{
+                // means 2nd half is sorted
+                mini = min(mini, nums[mid]);
+                e = mid-1;
             }
         }
         return mini;
