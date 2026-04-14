d635c6bd35b6415f04f1c9c497044df38ad7a9b7
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-14 18:36:48 +0000

    [LeetCode Sync] Runtime - 191 ms (46.92%), Memory - 132.7 MB (8.87%)

--- 0204-count-primes/solution.cpp
+++ 0204-count-primes/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
+    void markItsMultiples(int no, int n, vector<int> &primeArr){
+        for(int i=no*no; i<n; i+=no){
+            primeArr[i] = 1;
+        }
+    }
 public:
     int countPrimes(int n) {
-        // apna clg and kunal kushawaha app
-        vector<int> marked(n, 0);
-        for(int i=2; i*i <n; i++){
-            if(marked[i] == 0){
-                // for(int j=i*1; j<n; j+=i){
-                for(int j=i*2; j<n; j+=i){
-                    marked[j] = 1;
-                }
+        vector<int> primeArr(n, 0);
+
+        for(int i=2; i*i<n; i++){
+            if(primeArr[i] == 0){
+                markItsMultiples(i, n, primeArr);
             }
         }
 
-        // now we have marked 1 to all non-prime numbers
-        int cnt=0;
+        int cnt = 0;
         for(int i=2; i<n; i++){
-            if(marked[i] == 0) cnt++;
+            if(primeArr[i] == 0) cnt++;
         }
+
         return cnt;
     }
 };
