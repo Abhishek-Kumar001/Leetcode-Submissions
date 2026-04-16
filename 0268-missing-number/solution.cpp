d3239c066639792b9c3cd46aeb27ba31d05c61f1
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-16 18:06:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (39.23%)

--- 0268-missing-number/solution.cpp
+++ 0268-missing-number/solution.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-//     int missingNumber(vector<int>& nums) {
-        
-//         int i=0;
-//         while( i< nums.size()){
-//            int corr = nums[i];
-            
-//            if(nums[i] < nums.size()  && nums[i] != nums[corr]){
-//                swap( nums[i], nums[corr]);
-//            }
-//            else{
-//                i++;
-//            }
-//         }
-        
-//         for(int i=0; i<nums.size(); i++){
-//            if( nums[i] != i) return i;
-//         }
-//         return nums.size();
-//     }
-    
     int missingNumber(vector<int>& nums) {
-        
-        // method -2
-        int xor1=0, xor2=0;
-        for(int i=0; i<nums.size(); i++){
-           xor1^= nums[i];
-           xor2^=i;
+        int i=0, n = nums.size();
+
+        while(i < n){
+
+            if(nums[i] == n || nums[i] == i) i++;
+            else{
+                int correctInd = nums[i];
+                swap(nums[i], nums[correctInd]);
+            }
         }
-        xor2^=nums.size();
-        
-        return xor1 ^ xor2;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] != i) return i;
+        }
+        return n;
     }
 };
