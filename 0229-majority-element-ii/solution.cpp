10771ab071df3836d43deabc58600162f6d11d9e
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-16 17:15:38 +0000

    [LeetCode Sync] Runtime - 7 ms (40.15%), Memory - 26.4 MB (27.79%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-                //@ striver approach
-    
-        //since we have to find all nos which appears more than n/3 
-        // and algically there can be at max 2 such nos present at max
-
         unordered_map<int,int> mp;
         int reqTime = (nums.size()/3)+1;
         vector<int> ans;
         for(int it : nums){
+            //vvvi to not take duplicates eg = [2,2] or eg=[2,2,2,2,2]
             if( ans.size()!=0 && ans.back() == it){
                 continue;
             }
