3c4d538c702e0f7290a2879ba5a64b14f15e4b00
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-16 16:59:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (10.11%)

--- 0228-summary-ranges/solution.cpp
+++ 0228-summary-ranges/solution.cpp
@@ -2,48 +2,32 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
-        //base case 
-        if( nums.size() == 0) return ans;
+        if(nums.size() == 0) return ans;
+        string s = to_string(nums[0]);
+        ans.push_back(s);
 
-        int begin=nums[0];
-        int end = nums[0];
-        for( int i=1; i<nums.size(); i++){
-            if(nums[i-1] + 1  == nums[i] ){
-                //continue // we are in continu part
-            }
-            else{  // our prev range has ended
-               if( begin == end){
-                ans.push_back(to_string(begin));
-               }else{
-                  string temp = "";
-                  temp+=to_string(begin);
-                  temp+="->";
-                  temp+=to_string(end);
-
-                  ans.push_back(temp);
-               }
-               // since our prev range has ended & this time the new range start
-               // so store the begin no this new range in begin variable
-               begin = nums[i];
+        int prev = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == prev+1){
+                prev++;
+            }else{
+                if(stoi(ans.back()) == nums[i-1]){
+                    ans.push_back(to_string(nums[i]));
+                }else{
+                    ans[ans.size()-1] = ans.back() + "->" + to_string(prev);
+                    ans.push_back(to_string(nums[i]));
+                }
+                prev = nums[i];
             }
 
-            //every time our end must be updated b/c end variable hold 
-            // the last element of last range
-            end = nums[i];
+            if(i == nums.size()-1){
+                if(stoi(ans.back()) == nums[i]){
+                    // do nothing
+                }else{
+                    ans[ans.size()-1] = ans.back() + "->" + to_string(nums[i]);
+                }
+            }
         }
-
-        // pushing the last range explicetly
-        if( begin == end){
-            ans.push_back(to_string(begin));
-        }else{
-            string temp = "";
-            temp+=to_string(begin);
-            temp+="->";
-            temp+=to_string(end);
-
-            ans.push_back(temp);
-        }
-        
         return ans;
     }
 };
