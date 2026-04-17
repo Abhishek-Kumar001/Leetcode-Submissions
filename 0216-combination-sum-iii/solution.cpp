cbaa81f19a276fd7bb36fdd39690e8db4fae7915
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-17 15:54:47 +0000

    [LeetCode Sync] Runtime - 6 ms (5.22%), Memory - 14.4 MB (6.41%)

--- 0216-combination-sum-iii/solution.cpp
+++ 0216-combination-sum-iii/solution.cpp
@@ -1,38 +1,24 @@
 class Solution {
-    void helper(int val, int k, int req, vector<int> temp, vector<vector<int>> &ans){
-        
-       
-        //base case 1: we have k nos in temp vector
-        if(temp.size() == k ){
-            if( req == 0){
-                ans.push_back(temp);
-                return;
-            }
-            return ;            
+    void helper(int i, int k, int n, vector<int> temp, vector<vector<int>> &ans){
+
+        if(n == 0 && k == 0){
+            ans.push_back(temp);
+            return ;
         }
-        // base case 2
-        if(val > 9) return ;
+        if(i > 9) return ;
 
-        //base case 3: val > req;
-        if(val > req) return ;
-
-
-        // now we have 2 choices, take or not Take 
-        // take it
-        temp.push_back(val);
-        helper(val+1, k, req-val, temp, ans);
+        if(i > n || k < 0) return ;
+        
+        temp.push_back(i);
+        helper(i+1, k-1, n-i, temp, ans);
         temp.pop_back();
-
-        // not take option
-        helper(val+1, k, req, temp, ans);
-
+        helper(i+1, k, n, temp, ans);
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> temp;
         helper(1, k, n, temp, ans);
-
         return ans;
     }
 };
