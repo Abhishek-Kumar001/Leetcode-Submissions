9c732e3c6d8bbc29f33c9c99af9a61ec0f41349d
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-20 18:15:52 +0000

    [LeetCode Sync] Runtime - 1 ms (75.36%), Memory - 65 MB (69.99%)

--- 0287-find-the-duplicate-number/solution.cpp
+++ 0287-find-the-duplicate-number/solution.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int s=0, e=nums.size()-1;
-        while(s<=e){
-            int correctPosi = nums[s]-1;
-            if(nums[s] != nums[correctPosi]){
-                swap(nums[s], nums[correctPosi]);
-            }else{
-                s++;
+        // we will do cyclic sort and place all the numbers to its correct positions
+        int s = 0, e = nums.size();
+
+        while(s <= e-1){  // < or <= doesn't mattar
+            if(nums[s] == s+1) s++; // as it is at its correct position
+            else{
+                int correctInd = nums[s]-1;
+                // agar its correct index pe v wahi hai then this is duplicate
+                if(nums[s] == nums[correctInd]) return nums[s];
+                // else swap with its correct index value
+                swap(nums[s], nums[correctInd]);
             }
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] != i+1) return nums[i];
-        }
+        // now travers all the element of arr and find the number which is not
+        // on its correct position
+        for(int i=0; i<e; i++) if(nums[i] != i+1) return nums[i];
 
         return -1;
     }
