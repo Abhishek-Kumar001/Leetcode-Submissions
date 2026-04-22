c2ac18f3ab90e3e8be2243f0a79ca739c187eb12
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-22 08:07:59 +0000

    [LeetCode Sync] Runtime - 48 ms (26.65%), Memory - 86 MB (40.35%)

--- 0164-maximum-gap/solution.cpp
+++ 0164-maximum-gap/solution.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return 0;
-
+        int maxGap = 0;
         sort(nums.begin(), nums.end());
-        int maxDiff = 0;
-        for(int i=0; i<n-1; i++){
-            maxDiff = max(maxDiff, nums[i+1] - nums[i]);
+        for(int i=1; i<nums.size(); i++){
+            maxGap = max(maxGap, nums[i]-nums[i-1]);
         }
-        return maxDiff;
+        return maxGap;
     }
 };
