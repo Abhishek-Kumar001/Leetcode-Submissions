c58c3f2f22afcfc126274a71087c86fdf07b4d31
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-22 07:50:34 +0000

    [LeetCode Sync] Runtime - 107 ms (5.18%), Memory - 80 MB (5.27%)

--- 0041-first-missing-positive/solution.cpp
+++ 0041-first-missing-positive/solution.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    void swap(vector<int> &arr, int start, int end){
-        int temp= arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-    }
     int firstMissingPositive(vector<int>& nums) {
-        int start=0;
-        int end = nums.size();
-
-        while( start<end){
-            if(nums[start] == INT_MIN){  // since int can't store INT_MIN-1  in line no 16
-                start++; continue;
-            }
-            int correct = nums[start] -1;
-
-            if( nums[start]>0 && nums[start]<=end  &&  nums[start] != nums[correct]){
-                swap(nums, start, correct);
-            }
-            else{
-                start++;
-            }
-
-           
+        map<int, int> mpp;
+        for(int no : nums){
+            if(no > 0) mpp[no]++;
         }
-        for(int i=0; i<end; i++){
-                if( nums[i] != i+1){
-                    return i+1;
-                }
+
+        int cnt = 1;
+        for(auto it : mpp){
+            if(it.first != cnt) return cnt;
+            cnt++;
         }
-        return nums.size()+1;    
+        return cnt;
     }
 };
