0b3514f2a307a64606076ce4a8226c3915361593
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-04-24 06:55:10 +0000

    [LeetCode Sync] Runtime - 2 ms (5.14%), Memory - 8.7 MB (20.47%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+//     int helper(int n, vector<int> &dp){
+//         // base case 
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
+
+//         if(dp[n] != -1) return dp[n];
+
+//         int oneStair = helper(n-1, dp);
+//         int twoStair = helper(n-2, dp);
+//         return dp[n] = oneStair + twoStair;
+//     }
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+//         return helper(n, dp);
+//     }
+// };
+
+
 class Solution {
-    int helper(int n, vector<int> &dp){
-        if(n <= 0 ) return 0;
-        if(n == 1 || n == 2){
-            dp[n] = n;   // vvi if we are returning dp[n] not returning what helper return
-            return n;
-        } 
-
-        if(dp[n] != -1) return dp[n];
-
-        int oneStep = helper(n-1, dp);
-        int twoStep = helper(n-2, dp);
-
-        return dp[n] = oneStep + twoStep;
-    }
+   
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        helper(n, dp);
-        return dp[n];   
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+             int oneStair = dp[i-1];
+             int twoStair = dp[i-2];
+             dp[i] = oneStair + twoStair;
+        }
+
+        return dp[n];
     }
 };
