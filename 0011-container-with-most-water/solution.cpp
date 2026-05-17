3b63f42a5093908b3014ec70b2c58983e6c3551c
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-05-17 14:47:04 +0000

    [LeetCode Sync] Runtime - 1 ms (44.54%), Memory - 63.1 MB (11.34%)

--- 0011-container-with-most-water/solution.cpp
+++ 0011-container-with-most-water/solution.cpp
@@ -1,23 +1,3 @@
-/*
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        
-        // my  bruit force
-        int n = height.size();
-        int ans = INT_MIN;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int mini = min( height[i], height[j]);
-                int diff = j-i;
-                ans = max( ans, mini*diff);
-            }
-        }
-        return ans;
-    }
-};
-*/
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
