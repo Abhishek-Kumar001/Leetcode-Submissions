182d1a38e23958abb1aa2566e1c695eb4f0e1c7b
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-06-28 10:37:52 +0000

    [LeetCode Sync] Runtime - 5 ms (68.27%), Memory - 11.7 MB (77.10%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length() == 0) return 0;
         int start = 0;
-        int maxLen = 0;
-        vector<int> indexArr(256, -1);
+        unordered_map<char, int> mpp;
+        int maxLength = 0;
 
         for(int i=0; i<s.length(); i++){
             char ch = s[i];
-            if( indexArr[ch] != -1 && indexArr[ch] >= start){
-                // it means this character has came alrady and that too in after the start 
-                start = indexArr[ch]+1;
+            if(mpp.find(ch) != mpp.end()){
+                // it means it is this char is already there 
+                int occInd = mpp[ch];
+                if(occInd < start)  maxLength = max(maxLength , i-start+1); // do  nothing becuase we are already ahead 
+                else start = occInd +1;
+            }else{
+                // this char is coming for the 1st time
+                maxLength = max(maxLength , i-start+1);
             }
-            maxLen = max(maxLen, i-start+1);
-            indexArr[ch] = i;
+
+            mpp[ch] = i;
         }
-
-
-        return maxLen;
+        return maxLength;
     }
 };
