bd0e1ad6b471c278ea2e0c0cf6a8f79db0f44116
Abhishek Kumar <abhishekkrkumar310@gmail.com>
2026-07-29 16:51:56 +0000

    [LeetCode Sync] Runtime - 50 ms (60.85%), Memory - 139.1 MB (13.49%)

---
+++ 1023-time-based-key-value-store/solution.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+private:
+    unordered_map<string, map<int, string> > mpp;
+            //   <key, <timestamp, value>>
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mpp[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        if(mpp.find( key ) == mpp.end()) return "";
+
+        auto &timeline = mpp[key];
+        auto it = timeline.upper_bound(timestamp);
+
+        if (it == timeline.begin()) return "";     // no entry with time <= timestamp
+        --it;                                       // step back to the largest time <= timestamp
+        return it->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
